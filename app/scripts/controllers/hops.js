a4799b75472bc0a96ea5952e8de597506e2932ef
MKannisto <a1303389@haaga-helia.fi>
2015-05-07 11:13:10 +0300

    main

--- app/scripts/controllers/hops.js
+++ app/scripts/controllers/hops.js
@@ -7,12 +7,21 @@
  * # HopsCtrl
  * Controller of the huutoaanestysFrontApp
  */
-angular.module('huutoaanestysFrontApp')
+angular.module('huutoaanestysFrontApp')	    
   .controller('HopsCtrl', function ($scope, $http) {
-    
-	   $http.get('json/opiskelijat.json')
-	      .success(function(data) { //success returns a JS object
-	        $scope.opiskelija = data[0]; // bind the JS object to $scope
-	      });
-	    
+	  
+    /*
+	  $http.get('http://proto317.haaga-helia.fi:8080/huutoaanestys_back/opiskelijat.json')
+      .success(function(data) { //success returns a JS object
+        $scope.opiskelijat = data; // bind the JS object to $scope
+  });
+	  */
+	  
+	  $http.get('json/kurssit.json')
+      .success(function(data) { //success returns a JS object
+        $scope.opiskelija = data[0]; // bind the JS object to $scope
+  })
+  .error(function(error) {
+		  console.log('Virhe: hops.opiskelijat ' + error);
+		  });	  
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,21 @@
-angular.module('demoApp')
-	.controller('MainCtrl', function($scope) {
-		
-	});
+angular.module('huutoaanestysFrontApp')
+	.controller('MainCtrl', function($scope, $http) {
+		  
+		  $http.get('json/lukkarikurssit.json')
+	      .success(function(data) { //success returns a JS object
+	        $scope.lukkari = data; // bind the JS object to $scope  
+	        
+	  }),
+	  
+	  		  $http.get('json/tiedotteet.json')
+	      .success(function(data1) { //success returns a JS object
+	        $scope.tiedote = data1; // bind the JS object to $scope  
+	        
+	  })
 	
+	
+	  .error(function(error) {
+			  console.log('Virhe: main.opiskelija ' + error);
+			  });	  
+	  });
+
