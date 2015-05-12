2a853937296742d96efeb3023e0b8c1baf77ee8c
MKannisto <a1303389@haaga-helia.fi>
2015-05-12 11:42:20 +0300

    Päivitystä etusivulle.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,25 @@
+
 angular.module('huutoaanestysFrontApp')
 	.controller('MainCtrl', function($scope, $http) {
 		  
 		  $http.get('json/lukkarikurssit.json')
-	      .success(function(data) { //success returns a JS object
+	       .success(function(data) { //success returns a JS object
 	        $scope.lukkari = data; // bind the JS object to $scope  
 	        
 	  }),
 	  
 	  		  $http.get('json/tiedotteet.json')
-	      .success(function(data1) { //success returns a JS object
+	  	.success(function(data1) { //success returns a JS object
 	        $scope.tiedote = data1; // bind the JS object to $scope  
 	        
-	  })
-	
+	  }),
+	  
+	  	  
+	  	  	$http.get('json/kalenteri.json')
+	 	.success(function(data2) { //success returns a JS object
+	  	 $scope.deadline = data2; // bind the JS object to $scope  
+	        
+	})
 	
 	  .error(function(error) {
 			  console.log('Virhe: main.opiskelija ' + error);
